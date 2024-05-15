426c52dcdbab7bdba3b0f4f5ba53508d3ab1b0b3
henry <henryhu712@gmail.com>
2024-05-15 16:38:48 +0800

    workflows

---
+++ docs/04--workflows.md
@@ -0,0 +1,19 @@
+
+
+## 词汇教室
+
+### 创建
+
+准备图片：
+
+- Picture 480 x 270
+- 压缩(https://tinypng.com/)
+- 上传 R2
+
+创建房间：
+
+通过 API 创建词汇教室：
+
+    POST airoom/room
+
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-              Release 0.0.1 (2024.05.10)
+              Release 0.0.1 (2024.05.14)
           </Link>
         </div>
       </div>
